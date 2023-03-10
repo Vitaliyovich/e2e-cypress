4bd0df487e72b773a7a04bf42b31f9e9771dda80
Vitalii <108929784+Vitaliyovich@users.noreply.github.com>
2023-03-10 13:08:01 +0200

    homework playwright ui v2

--- playwright/tests/ui/homework.spec.ts
+++ playwright/tests/ui/homework.spec.ts
@@ -1,20 +1,8 @@
 import { test, expect } from '@playwright/test';
 
 const S = {
-  title: '[data-cy="title"]',
-  menuLink: 'p-menuitem-link',
-  homeLink: '[data-automationid="home-page"]',
-  todoLink: '[data-automationid="todo-page"]',
-  postsLink: '[data-automationid="posts-page"]',
-  galleryLink: '[data-automationid="gallery-page"]',
-  aboutLink: '[data-automationid="about-page"]',
   inputTodo: '[data-cy="todo-text"]',
   addButton: '[data-cy="add-btn"]',
-  deleteButton: '[data-cy="delete-btn"]',
-  editButton: '[data-cy="edit-btn"]',
-  editText: '[data-cy="edit-text"]',
-  saveButton: '[data-cy="save-btn"]',
-  todoLabel: '[data-cy="label"]',
   todoItem: '[data-cy="todo-item"]',
   activeButton: '[data-cy="show-active-btn"]',
   inActiveButton: '[data-cy="show-inactive-btn"]',
@@ -47,24 +35,16 @@ test.describe('Todo', () => {
     await page.locator('[data-cy="todo-item"]:nth-child(1) .p-checkbox').click();
     await page.locator('[data-cy="todo-item"]:nth-child(2) .p-checkbox').click();
 
-    await page.pause();
-
     await page.locator(S.activeButton).click();
     const todoItemsActive = await page.locator(S.todoItem).count();
     await expect(todoItemsActive).toBe(3);
-
-    await page.pause();
 
     await page.locator(S.inActiveButton).click();
     const todoItemsInActive = await page.locator(S.todoItem).count();
     await expect(todoItemsInActive).toBe(2);
 
-    await page.pause();
-
     await page.locator(S.allTodoButton).click();
     const todoItemsAll = await page.locator(S.todoItem).count();
     await expect(todoItemsAll).toBe(5);
-
-    await page.pause();
   });
 });
